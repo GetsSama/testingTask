45b828e93c291b592d958076be42ec13c99c355b
GetsuGet <zurik.n@gmail.com>
2022-08-06 17:52:39 +0300

    Worked realization for first rule.

--- src/main/java/com/gridnine/testing/FlightFilter.java
+++ src/main/java/com/gridnine/testing/FlightFilter.java
@@ -27,7 +27,6 @@ public class FlightFilter {
                 filteredList.add(flight);
         }
 
-        flights = filteredList;
         return filteredList;
     }
 }
--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -6,12 +6,13 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Flight> flights = FlightBuilder.createFlights();
-        List<String> rules = Arrays.asList("DepartureBeforeNow");
+        String rule1 = "DepartureBeforeNow";
+
         System.out.println(flights);
 
         FlightFilter filter1 = new FlightFilter();
-        //filter1.filter(flights, rules);
-        System.out.println(filter1.filter(flights, rules));
-        System.out.println(flights);
+
+        System.out.println(filter1.filter(flights, Arrays.asList(rule1)));
+
     }
 }
