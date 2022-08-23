19398e94d4c9cfc1e28f1c9586c21efe9c5791cb
GetsuGet <zurik.n@gmail.com>
2022-08-23 15:42:57 +0300

    Dynamic class load worked.

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -26,25 +26,5 @@ public class Main {
         System.out.println(filter1.filter(rule1).filter(rule2).filter(rule3).getFilteredList());
         //System.out.println(filter1.filter(rule1, rule2, rule3).getFilteredList());
         //System.out.println(filter1.filter(Arrays.asList(rule1, rule2, rule3)).getFilteredList());
-
-
-        List<Class<?>> classes = ClassFinder.find("com.gridnine.testing");
-        /*for (Class cls : classes)
-            System.out.println(cls.getSimpleName());*/
-
-        for (Class clazz : classes){
-            String superClassName = "";
-            try {
-            superClassName = clazz.getSuperclass().getSimpleName();
-            } catch (NullPointerException e) {}
-
-            if (superClassName.equals("SimpleRuleImpl") || superClassName.equals("AttributedRule"))
-                System.out.println(clazz.getSimpleName());
-        }
-
-        System.out.println(classes.get(0).getMethod("getInstance").invoke(null));
-        System.out.println(classes.get(0).getMethod("getInstance").invoke(null));
-        System.out.println(classes.get(0).getMethod("getInstance").invoke(null));
-
     }
 }
